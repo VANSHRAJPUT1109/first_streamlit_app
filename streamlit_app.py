344e1a50d755ccbe4c01222a15439a60678792b8
VANSHRAJPUT1109 <112627468+VANSHRAJPUT1109@users.noreply.github.com>
2022-09-01 17:36:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Healthy Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('Kale, Spinach & Rocket Soothie')
+
+streamlit.text('Chicken Sausage')
+
+
